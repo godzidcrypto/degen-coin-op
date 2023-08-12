6a2466cf5112c3d50dd00b2cdec86a29bc95a20d
godzidcypto <godzidcrypto@gmail.com>
2023-08-13 03:18:21 +1200

    added nav transition on scroll

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const Navbar = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="text-white flex justify-between py-4 px-12 fixed w-full items-center z-20">
+    <nav
+      className={`text-white flex justify-between py-8 px-12 fixed w-full items-center z-40 ${
+        scrolling
+          ? "bg-gray-950 transition duration-300 ease-in-out"
+          : "bg-transparent transition duration-300 ease-in-out"
+      }`}
+    >
       <div className="flex gap-16 font-medium">
         <p>Logo</p>
         <p>Games</p>
@@ -8,7 +36,7 @@ const Navbar = () => {
         <p>Degen Fat Cats</p>
         <p>Careers</p>
       </div>
-      <button className="bg-[#312966] rounded-full p-2">
+      <button className="rounded-full p-2 bg-[#312966]">
         Download Phantom
       </button>
     </nav>
